1e9afd2d5e36f684b0499200fabca8a1936bf38e
LorisQ <121116287+LorisQsd@users.noreply.github.com>
2023-03-21 12:22:25 +0100

    test commit
    Amélioration responsive design

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ $(function(){
         $("#hideInfo").slideToggle("slow");
     });
 
+    $("#buttonNav").click(function(){
+        $("#hideUl").slideToggle("slow");
+        });
+    // $(".hideOnClick").click(function() {
+    //     $("#hideUl").hide("slow");
+    // });
 
+
+
+    
 });
-
